0fb3f831085690ceb171573cf19c55186495070b
AliNabil7 <65635613+AliNabil7@users.noreply.github.com>
2022-08-25 01:35:15 +0200

    Uploaded the 1st LinkedIn Course challenges

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
@@ -1,1 +1,6 @@
+def favorite_city(name):
+  print("One of my favorite cities is", name)
 
+favorite_city("Paris")
+favorite_city("Montreal")
+favorite_city("Baku")
--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
@@ -1,1 +1,8 @@
+userGuess = input("What's my favorite food? ")
 
+if userGuess == 'steak':
+  print('Yep! So amazing!')
+else:
+  print("Yuck! That's not it!")
+
+print('Thanks for playing!')
